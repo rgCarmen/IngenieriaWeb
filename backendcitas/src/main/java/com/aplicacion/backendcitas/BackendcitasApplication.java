e1023f03047aaa3347fb1f51c08c5b080cc46d0b
carmen <carmenrodriguezgonzalez0822@gmail.com>
2024-11-21 16:35:41 +0100

    Spring Boot directorio backend

---
+++ backendcitas/src/main/java/com/aplicacion/backendcitas/BackendcitasApplication.java
@@ -0,0 +1,13 @@
+package com.aplicacion.backendcitas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendcitasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendcitasApplication.class, args);
+	}
+
+}
