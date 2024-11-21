06056f22a35cd455054d8e4534355aacd524907f
carmen <carmenrodriguezgonzalez0822@gmail.com>
2024-11-21 16:27:32 +0100

    spring Boot project

---
+++ appcitas/src/test/java/com/aplicacioncitas/appcitas/AppcitasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aplicacioncitas.appcitas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppcitasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
