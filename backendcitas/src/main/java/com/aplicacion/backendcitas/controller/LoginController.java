225e3a58c2286a64307a381f168dc907901b69fb
Gonzalo_MA <gonzalo.mesas.aranda@gmail.com>
2024-12-04 16:31:18 +0100

    MEdico insertado

---
+++ backendcitas/src/main/java/com/aplicacion/backendcitas/controller/LoginController.java
@@ -0,0 +1,28 @@
+package com.aplicacion.backendcitas.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.aplicacion.backendcitas.model.Cita;
+import com.aplicacion.backendcitas.model.CitaService;
+
+@RestController
+public class LoginController {
+
+    
+
+
+
+
+}
