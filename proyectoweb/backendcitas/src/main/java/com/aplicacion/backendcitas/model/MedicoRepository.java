d0aa0563fc0869891104d525056afdbadfa25877
Ainhoa <160257181+ainhoapergon@users.noreply.github.com>
2024-12-10 20:13:47 +0100

    Delete proyectoweb directory

--- proyectoweb/backendcitas/src/main/java/com/aplicacion/backendcitas/model/MedicoRepository.java
+++
@@ -1,8 +0,0 @@
-package com.aplicacion.backendcitas.model;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface MedicoRepository extends JpaRepository<Medico, Long> {
-}
